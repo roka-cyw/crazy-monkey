9aa5b66a938eb9d17ae39c592f31fd761fc2cf89
Roka <kazakov302215@gmail.com>
2024-07-11 03:56:42 -0600

    added settings page (draft)

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Image, Pressable } from 'react-native'
+import { View, Image } from 'react-native'
 import { useRouter } from 'expo-router'
 
 import SharedScreen from '@/components/SharedScreen'
@@ -20,30 +20,25 @@ const Menu = () => {
     require('@/assets/buttons/rules.png')
   ]
 
-  // const handleButtonClick = item => router.push(`/${item}`)
+  const handleButtonClick = (item: string) => () => {
+    // router.push(`/${item}`)
+    console.log(`${item}`)
+  }
+
+  // It has to be modal
+  const handleSettingsClick = () => router.push(`/settings`)
 
   return (
     <SharedScreen isDarkened={false}>
       <View className='flex-1'>
-        {/* <Pressable onPress={() => console.log('pres setings')}> */}
-        <CustomButton
-          image={settings}
-          onPress={() => console.log('pres setings')}
-          containerStyles='absolute top-4 left-4 z-10'
-        />
-        {/* </Pressable> */}
+        <CustomButton image={settings} onPress={handleSettingsClick} containerStyles='absolute top-4 left-4 z-10' />
         <View className='flex-1 justify-between items-center'>
           <Image source={logo} resizeMode='cover' className='mt-16 ' style={{ width: 200, height: 200 }} />
         </View>
 
         <View className='flex-1 justify-around items-center bottom-1'>
           {btns.map((item, index) => (
-            <CustomButton
-              key={item.toString()}
-              image={images[index]}
-              // onPress={handleButtonClick(item)}
-              onPress={() => console.log(item)}
-            />
+            <CustomButton key={item.toString()} image={images[index]} onPress={handleButtonClick(item)} />
           ))}
         </View>
       </View>
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Settings = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text>Settings</Text>
+    </View>
+  )
+}
+
+export default Settings
