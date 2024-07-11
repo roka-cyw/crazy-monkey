6c171f5792b3e796b6307223384924169459beaa
Roka <kazakov302215@gmail.com>
2024-07-11 03:06:37 -0600

    added settings btn in the menu

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { View, Image } from 'react-native'
+import { View, Image, Pressable } from 'react-native'
 import { useRouter } from 'expo-router'
 
 import SharedScreen from '@/components/SharedScreen'
 import CustomButton from '@/components/CustomButton'
 
 import logo from '@/assets/logo.png'
+import settings from '@/assets/buttons/settings.png'
 
 const Menu = () => {
   const router = useRouter()
@@ -24,6 +25,13 @@ const Menu = () => {
   return (
     <SharedScreen isDarkened={false}>
       <View className='flex-1'>
+        {/* <Pressable onPress={() => console.log('pres setings')}> */}
+        <CustomButton
+          image={settings}
+          onPress={() => console.log('pres setings')}
+          containerStyles='absolute top-4 left-4 z-10'
+        />
+        {/* </Pressable> */}
         <View className='flex-1 justify-between items-center'>
           <Image source={logo} resizeMode='cover' className='mt-16 ' style={{ width: 200, height: 200 }} />
         </View>
