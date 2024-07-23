ebe398dd8b949fec75227f4bbc23647043e61caf
Roka <kazakov302215@gmail.com>
2024-07-23 00:11:05 -0600

    Ground linter fixes

--- components/GroundPiece.tsx
+++ components/GroundPiece.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { Image, SkImage } from '@shopify/react-native-skia'
-import { SharedValue } from 'react-native-reanimated'
+import { SharedValue, useDerivedValue } from 'react-native-reanimated'
 
-interface Props {
+interface Props extends xAnimationProps {
   ground: SkImage | null
   width: number
   height: number
-  x: SharedValue<number>
+  arrIndex: number
   y: number
 }
 
-const GroundPiece: React.FC<Props> = ({ ground, width, height, x, y }) => {
-  if (!ground) return null
+interface xAnimationProps {
+  totalWidth: number
+  groundWidth: number
+  animationProgress: SharedValue<number>
+}
+
+const GroundPiece: React.FC<Props> = ({ ground, width, height, arrIndex, totalWidth, groundWidth, animationProgress, y }) => {
+  const x = useDerivedValue(() => {
+    const position = (-totalWidth * animationProgress.value + arrIndex * groundWidth) % totalWidth
+    return position < -groundWidth ? position + totalWidth : position
+  })
 
   return <Image image={ground} width={width} height={height} x={x} y={y} />
 }
--- components/Grounds.tsx
+++ components/Grounds.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { useSharedValue, withRepeat, withTiming, useDerivedValue, Easing, cancelAnimation, DerivedValue } from 'react-native-reanimated'
+import React, { useCallback, useEffect } from 'react'
+import { useSharedValue, withRepeat, withTiming, Easing, cancelAnimation } from 'react-native-reanimated'
 import { useImage } from '@shopify/react-native-skia'
 
 import GroundPiece from './GroundPiece'
@@ -27,27 +27,21 @@ const Ground: React.FC<Props> = ({ width, height, isStartGame }) => {
   const numImages = Math.ceil(width / groundWidth) + EXTRA_GROUNDS
   const totalWidth = groundWidth * numImages
 
+  const xAnimationProps = {
+    totalWidth,
+    groundWidth,
+    animationProgress
+  }
+
+  const moveGrounds = useCallback(() => {
+    animationProgress.value = withRepeat(withTiming(1, { duration: ANIMATION_DURATION, easing: Easing.linear }), -1, false)
+  }, [animationProgress])
+
   useEffect(() => {
     if (isStartGame) moveGrounds()
 
     return () => cancelAnimation(animationProgress)
   }, [isStartGame, animationProgress, moveGrounds])
-
-  const moveGrounds = () => {
-    animationProgress.value = withRepeat(withTiming(1, { duration: ANIMATION_DURATION, easing: Easing.linear }), -1, false)
-  }
-
-  // const calculatePosition = useDerivedValue(() => {
-  //   const position = (-totalWidth * animationProgress.value + index * groundWidth) % totalWidth
-  //   return position < -groundWidth ? position + totalWidth : position
-  // })
-
-  const calculatePosition = (index: number): DerivedValue<number> => {
-    return useDerivedValue(() => {
-      const position = (-totalWidth * animationProgress.value + index * groundWidth) % totalWidth
-      return position < -groundWidth ? position + totalWidth : position
-    })
-  }
 
   // TODO: change 6 to numImages. Now these numbers are equal but what if user use iPad?
   // for solving I have to get the  actual index[from 0 to 6] inside commented calculatePosition()
@@ -57,8 +51,9 @@ const Ground: React.FC<Props> = ({ width, height, isStartGame }) => {
       ground={ground}
       width={groundWidth}
       height={groundHeight}
-      x={calculatePosition(index)}
+      arrIndex={index}
       y={height - groundHeight / 1.5}
+      {...xAnimationProps}
     />
   ))
 }
