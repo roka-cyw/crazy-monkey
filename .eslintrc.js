492fb859041b1a37d953a5e2bf4b6534c2e29f00
Roka <kazakov302215@gmail.com>
2024-07-22 19:45:37 -0600

    added prettier and linter

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'prettier'],
+  plugins: ['react', 'react-native', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-native/no-unused-styles': 'warn',
+    'react-native/split-platform-components': 'warn',
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-color-literals': 'warn',
+    'react-native/no-raw-text': ['warn', { skip: ['CustomText'] }]
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  formatOnSave: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  semi: false,
+  trailingComma: 'none',
+  printWidth: 140,
+  singleQuote: true
+}
